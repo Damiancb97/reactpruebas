3a15766226af186bda5d614bd8254286495b82d4
Damian <damiancb97@gmail.com>
2024-12-14 22:25:07 +0100

    seleccionar jugador1 limitado a 6

--- src/games/CombatPokemon.js
+++ src/games/CombatPokemon.js
@@ -3,14 +3,29 @@ import Pokemon from "../components/pokeapi/Pokemon";
 
 
 const CombatPokemon = (props) => {
+    const pokeArray = [];
+    const PokeRival = [];
+
+    const handleSelect = ({ pokeId, pokeArray }) => {
+        if (pokeArray.length < 6) {
+            pokeArray.push(pokeId);
+            console.log(pokeId);
+        }
+    }
+
+    const arrayRival = ({ pokeId, pokeRivel }) => {
+        while (newRivalTeam.length < 6) {
+            const randomId = Math.floor(Math.random() * 151) + 1;
+            if (!newRivalTeam.includes(randomId)) {
+                newRivalTeam.push(randomId);
+            }
+        }
+    }
 
     return <>
+        {/* <Pokemon /> */}
 
-        <Pokemon />
-
-        {/* <br></br>
-        <h2>POKELISTA</h2>
-        <PokeList /> */}
+        <PokeList selectPokemon={handleSelect} selectedPokemons={pokeArray} />
 
     </>
 }
